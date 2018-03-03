cf90e87b32b770a6930eeea3e23c15775f8fb308
pradnyeshp <pradnyesh.patil@sjsu.edu>
2018-03-02 21:24:23 -0800

    handleChange & handleSubmit added
    Added new Functions named handleChange & handleSubmit and Updated the form

--- lab1/freelancerprototype/src/Components/SignUp.js
+++ lab1/freelancerprototype/src/Components/SignUp.js
@@ -2,25 +2,57 @@ import React, { Component } from 'react';
 import './App.css';
 
 class SignUp extends Component {
+
+    constructor(){
+        super();
+        this.state={
+            email: '',
+            username: '',
+            pwd: ''
+        }
+        this.handleChange.bind(this)
+    }
+
+    handleChange = (e) => {
+
+        const name = e.target.name
+        const value = e.target.value
+
+        this.setState({
+            [name] : value
+        })
+
+        console.log(this.state);
+        
+    }
+
+    handleSubmit(e){
+        
+    }
+    
     render() {
         return (
             <div className="signup">
-                <form>
-                    <div class="form-group">
+                <form onSubmit={this.handleSubmit}>
+                    <h2>Please enter you details</h2><br/><br/>
+                    <div className="form-group">
                         <label>Email:</label>
-                        <input type="text" class="form-control" ref="email" required/>
+                        <input type="text" className="form-control" name="email" placeholder='Email Address' 
+                            onChange={this.handleChange} required/>
                     </div>
-                    <div class="form-group">
+                    <div className="form-group">
                         <label>Name:</label>
-                        <input type="text" class="form-control" ref="name" required />
+                        <input type="text" className="form-control" name="username" placeholder='Username' 
+                            onChange={this.handleChange} required />
                     </div>
-                        <div class="form-group">
+                        <div className="form-group">
                             <label>Password:</label>
-                        <input type="password" class="form-control" ref="pwd" required/>
+                        <input type="password" className="form-control" name="pwd" placeholder='Password' 
+                            onChange={this.handleChange} required/>
                     </div>    
-                    <label class="radio-inline"><input type="radio" name="hire" />Hire</label> &nbsp;&nbsp;
-                    <label class="radio-inline"><input type="radio" name="work"/>Work</label> <br/> <br/>
-                    <button className="btn btn-primary">Create Account</button>
+                    <label className="radio-inline"><input type="radio" name="hire" /> Hire </label> &nbsp;&nbsp;
+                    <label className="radio-inline"><input type="radio" name="work"/> Work </label> <br/> <br/>
+                    <button className="btn btn-primary"> Create Account</button>
                 </form>
             </div>
         );
