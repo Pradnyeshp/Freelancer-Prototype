4a833ae13b64807f4f2024f567ec7d8453c007a2
pradnyeshp <pradnyesh.patil@sjsu.edu>
2018-03-02 18:38:31 -0800

    Added Operations
    Performed operations on Server with data received from Client-side and sending responce back to Client with Result of the operation

--- lab1/calculatorserver/routes/index.js
+++ lab1/calculatorserver/routes/index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
+
   res.render('index', { title: 'Express' });
+
 });
 
 router.post('/operations', function (req, res, next) {
@@ -11,19 +13,19 @@ router.post('/operations', function (req, res, next) {
 
     case 'add': 
       const result1 = Number(req.body.op1) + Number(req.body.op2)
-      res.json(result1)
+      res.json(result1);
       
     case  'sub':
       const result2 = Number(req.body.op1) - Number(req.body.op2)
-      res.json(result2)
+      res.json(result2);
       
     case 'mul':
       const result3 = Number(req.body.op1) * Number(req.body.op2)
-      res.json(result3)
+      res.json(result3);
       
     case 'div':
       const result4 = Number(req.body.op1) / Number(req.body.op2)
-      res.json(result4)
+      res.json(result4);
       
   }
 });
