7c805150aca15085700125c75e9cd73fa768efa6
pradnyeshp <pradnyesh.patil@sjsu.edu>
2018-03-03 00:10:11 -0800

    Added Functions : handleChange and handleSubmit
    Added new functions handleChange and handleSubmit and made some minor UI changes

--- lab1/freelancerprototype/src/Components/SignIn.js
+++ lab1/freelancerprototype/src/Components/SignIn.js
@@ -2,20 +2,55 @@ import React, { Component } from 'react';
 import './App.css';
 
 class SignIn extends Component {
+
+    constructor(){
+        super();
+        this.state={
+            email : '',
+            pwd : ''
+        }
+        this.handleChange.bind(this)
+        this.handleSubmit.bind(this)
+    }
+
+    handleChange = (e) => {
+
+        const name = e.target.name
+        const value = e.target.value
+
+        this.setState({
+            [name]: value
+        })
+        console.log(this.state);
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+
+        const userData = {
+            email: this.state.email,
+            pwd: this.state.pwd
+        }
+        console.log(userData);
+    }
+    
     render() {
         return (
-            <div className="headerDiv">
-                <div className="modal-header">
-                    <h2 className="modal-header-logo">Logo here</h2><br />
+            <div className="center">
+                <div>
+                    <h2 className="modal-header-logo">Logo here</h2> <br />
+                    <h3>Login</h3><br/>
                 </div>
-                <form>
+                <form onSubmit={this.handleSubmit} >
                     <div className="form-group">
                         <label>Email:</label>
-                        <input type="text" className="form-control" ref="email" placeholder="Email or Username" required />
+                        <input type="text" className="form-control" name="email" 
+                            placeholder="Email or Username" onChange={this.handleChange} required />
                     </div>
-                    <div class="form-group">
-                        <label for="pwd">Password:</label>
-                        <input type="password" className="form-control" ref="pwd" placeholder="Password" required />
+                    <div className="form-group">
+                        <label>Password:</label>
+                        <input type="password" className="form-control" name="pwd" 
+                            placeholder="Password" onChange={this.handleChange} required />
                     </div>
                     <button className="btn btn-primary" >Sign In</button>
                 </form>
