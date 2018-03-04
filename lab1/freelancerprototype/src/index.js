24ae7ebcc6ccd2b13f0a6d8f570292ca301934c8
pradnyeshp <pradnyesh.patil@sjsu.edu>
2018-03-03 17:59:46 -0800

    Configured Store with Provider
    Configured Store with Provider

--- lab1/freelancerprototype/src/index.js
+++ lab1/freelancerprototype/src/index.js
@@ -5,11 +5,17 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter } from 'react-router-dom';
+import Store from './Store';
+import { Provider } from 'react-redux';
+
+const store = Store();
 
 ReactDOM.render(
-<BrowserRouter>
-    <App />
-</BrowserRouter>, 
+<Provider store={store}>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+</Provider>, 
 document.getElementById('root'));
 
 registerServiceWorker();
