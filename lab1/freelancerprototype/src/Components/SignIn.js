c13c497ffd017457fc8861ac065c67179b5a3859
pradnyeshp <pradnyesh.patil@sjsu.edu>
2018-03-05 16:37:31 -0800

    few changes
    few changes

--- lab1/freelancerprototype/src/Components/SignIn.js
+++ lab1/freelancerprototype/src/Components/SignIn.js
@@ -1,96 +1,97 @@
 import React, { Component } from 'react';
-import './App.css';
-import { axios } from 'axios' ;
-import { Redirect } from 'react-router-dom' ;
-import { connect } from 'react-redux' ;
+import axios from 'axios';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+
 
 class SignIn extends Component {
-
-    constructor(){
+    constructor() {
         super();
-        this.state={
-            email : '',
-            pwd : ''
+        this.state = {
+            username: "",
+            password: "",
+            error: ""
         }
-        this.handleChange.bind(this)
-        this.handleSubmit.bind(this)
     }
 
     handleChange = (e) => {
-
-        const name = e.target.name
-        const value = e.target.value
-
         this.setState({
-            [name]: value
+            [e.target.name]: e.target.value
         })
-        console.log(this.state);
     }
 
-    handleSubmit = (e) => {
-        e.preventDefault()
-
+    handleSignIn = (e) => {
+        e.preventDefault();
         const userData = {
-            email: this.state.email,
-            pwd: this.state.pwd
+            username: this.state.username,
+            password: this.state.password
         }
         console.log(userData);
-        this.props.login(userData);
+        this.props.loginUser(userData);
     }
-    
+
     render() {
-        let profile = null ;
-        if(this.props.login !== null){
-            profile = <Redirect to = '/profile' />
+        let profile = null;
+        if (this.props.loginData !== null) {
+            profile = <Redirect to="/Profile" />
         }
 
         return (
-            <div className="center">
-                <div>
-                    <h2 className="modal-header-logo">Logo here</h2> <br />
-                    <h3>Login</h3><br/>
+            <div className="Login">
+                {profile}
+                <div id="mainDiv">
+                    <div className="center">
+                        <div>
+                            <h1> Login  </h1>
+                        </div>
+                        <div id="divLoginForm">
+                            <form onSubmit={this.handleSignIn.bind(this)}>
+                                <div className="form-group">
+                                    <input type="text" value={this.state.username} onChange={this.handleChange} className="form-control" id="txtUserName" placeholder="Email or Username" ref="uname" name="username" required />
+                                </div>
+                                <div className="form-group">
+                                    <input type="password" value={this.state.password} onChange={this.handleChange} className="form-control" id="txtPassword" placeholder="Enter Password" ref="pass" name="password" required />
+                                </div>
+                                <div className="form-group">
+                                    <input type="submit" className="form-control btn btn-primary" id="btnSubmitSignUpForm" value="Login" />
+                                </div>
+
+                            </form>
+                        </div>
+                    </div>
                 </div>
-                <form onSubmit={this.handleSubmit} >
-                    <div className="form-group">
-                        <label>Email:</label>
-                        <input type="text" className="form-control" name="email" 
-                            placeholder="Email or Username" onChange={this.handleChange} required />
-                    </div>
-                    <div className="form-group">
-                        <label>Password:</label>
-                        <input type="password" className="form-control" name="pwd" 
-                            placeholder="Password" onChange={this.handleChange} required />
-                    </div>
-                    <button className="btn btn-primary" >Sign In</button>
-                </form>
+
             </div>
         );
     }
 }
 
-
 function mapStateToProps(state) {
     return {
         username: state.username,
-        pwd: state.pwd,
-        logindata: state.logindata
-    };
+        password: state.password,
+        loginData: state.login_data
+    }
 }
 
 function mapDispatchToProps(dispatch) {
     return {
-        login: (userData) => {
+        loginUser: (userData) => {
+            console.log("In Login dispatch", userData);
             axios.post('http://localhost:3001/signin', userData)
                 .then((response) => {
-                    console.log(response.data[0]);
-                    if (response.data === 'ERROR') {
-                        dispatch({type: 'ERROR', payload: response })
-                    }
-                    else
+                    //console.log(response);
+                    console.log("After login dispatch", response.data);
+                    if (response.data === 'ERROR')
+                        dispatch({ type: 'ERROR', payload: response })
+                    else {
+                        //sessionStorage.setItem('username', response.data[0].username)
                         dispatch({ type: 'SIGNIN_SUCCESS', payload: response })
+                    }
+
                 });
         }
-    };
+    }
 }
 
-export default (mapStateToProps,mapDispatchToProps) (SignIn);
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
