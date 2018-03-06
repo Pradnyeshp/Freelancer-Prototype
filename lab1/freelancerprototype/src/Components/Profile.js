7c96bc2296e7acaf338583210ab83d996ecb950a
pradnyeshp <pradnyesh.patil@sjsu.edu>
2018-03-05 17:44:15 -0800

    Few UI improvements
    Few UI improvements

--- lab1/freelancerprototype/src/Components/Profile.js
+++ lab1/freelancerprototype/src/Components/Profile.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import {Link} from 'react-router-dom';
 
 class Profile extends Component {
     render() {
@@ -7,24 +8,25 @@ class Profile extends Component {
             <div className="profile">
                 <form>
                     <label>Name :
-                        <input type="text" ref="name" required />
-                    </label>
+                        <input type="text" ref="name"  />
+                    </label><br/>
                     <label>Email :
-                        <input type="text" ref="email" required />
-                    </label>
+                        <input type="text" ref="email"  />
+                    </label><br />
                     <label>Phone Number :
-                        <input type="text" ref="phone" required />
-                    </label>
+                        <input type="text" ref="phone"  />
+                    </label><br />
                     <label>About Me :
-                        <input type="text" ref="aboutme" required />
-                    </label>
+                        <input type="text" ref="aboutme"  />
+                    </label><br />
                     <label>Skills :
-                        <input type="text" ref="email" required />
-                    </label>
+                        <input type="text" ref="email"  />
+                    </label><br />
                     <label>Profile Image :
-                        <input type="text" ref="email" required />
-                    </label>
+                        <input type="text" ref="email"  />
+                    </label><br />
                     <button>Edit</button>
+                    <Link to='/' className='btn btn-danger'>LogOut</Link>
                 </form>
             </div>
         );
