bef9611f9c9cbcb201d0471e9d0b3728b9467ba2
pradnyeshp <pradnyesh.patil@sjsu.edu>
2018-03-06 15:59:17 -0800

    UI Tweaks
    tweaks using css

--- lab1/freelancerprototype/src/Components/Navigator.js
+++ lab1/freelancerprototype/src/Components/Navigator.js
@@ -1,27 +1,29 @@
 import React, { Component } from 'react';
-import './App.css';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import {Link} from 'react-router-dom'
+import './navigator.css'
 
 class Navigator extends Component {
     render() {
         return (
             <div className="container-fluid" >
-                <nav className="navbar navbar-inverse" >
-                    <div className="container-fluid">
-                        <div className="navbar-header">
-                            <a className="navbar-brand"> Freelancer Logo </a>
+                    <nav className="navbar navbar-inverse" >
+                        <div className="container-fluid">
+                            <div className="navbar-header">
+                                <a className="navbar-brand"> Freelancer Logo </a>
+                            </div>
+                            <ul className="nav navbar-nav navbar-right" >
+                                <li><Link to="/SignIn"><font color="black"> Login </font></Link></li>
+                            </ul>
+                            <ul className="nav navbar-nav navbar-right">
+                                <li><p><Link to="/SignUp"><font color="black"> Sign Up </font></Link></p></li>
+                            </ul>
+                            <ul className="nav navbar-nav navbar-right">
+                                <li><Link to="/AddProject" className="btn btn-primary"> Post a Project </Link></li>
+                            </ul>
                         </div>
-                        <ul className="nav navbar-nav navbar-right" styles={{ float: 'right', paddingRight: '5px' }}>
-                            <li><Link to="/SignIn"> Login </Link></li>
-                        </ul>
-                        <ul className="nav navbar-nav navbar-right">
-                            <li><Link to="/SignUp"> Sign Up </Link></li>
-                        </ul>
-                        <ul className="nav navbar-nav navbar-right">
-                            <li><Link to="/AddProject" className="btn btn-primary"> Post a Project </Link></li>
-                        </ul>
-                    </div>
-                </nav>
+                    </nav>
+                
             </div>
         );
     }
