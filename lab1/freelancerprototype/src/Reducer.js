c424d7dee4b7844f9578eecf76428239e9a221e3
pradnyeshp <pradnyesh.patil@sjsu.edu>
2018-03-05 16:38:44 -0800

    Added SignIn_Success case
    Added SignIn_Success case

--- lab1/freelancerprototype/src/Reducer.js
+++ lab1/freelancerprototype/src/Reducer.js
@@ -8,10 +8,16 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     
         switch (action.type) {
-            case 'SIGNUP_SUCCESS': 
-                return{ 
+            case 'SIGNUP_SUCCESS' : 
+                return { 
                     ...state,
                     signup_success : action.payload.data
+                }
+
+            case 'SIGNIN_SUCCESS' :
+                return {
+                    ...state,
+                    signin_success : action.payload.data
                 }
         
             default:
