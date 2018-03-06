030c40dcbfed4b0100c97ec820ef062536db0a23
pradnyeshp <pradnyesh.patil@sjsu.edu>
2018-03-05 16:36:53 -0800

    Added 404 check
    Added 404 check

--- lab1/freelancerprototype/src/Components/Home.js
+++ lab1/freelancerprototype/src/Components/Home.js
@@ -9,17 +9,20 @@ import SignUp from './SignUp';
 import AddProject from './AddProject';
 
 const Home = () => (
+  <div>
     <Switch>
       <Route exact path = '/' component = {Navigator} />
-      <Route path = '/SignIn' component = {SignIn} />
-      <Route path = '/SignUp' component = {SignUp} />
+      <Route path = '/signin' component = {SignIn} />
+      <Route path = '/signup' component = {SignUp} />
       <Route path = '/AddProject' component = {AddProject} />
-      <Route path='*' component={NotFound} /> 
+      <Route path='*' component={NotFound} />
     </Switch>
+  </div>
 )
 
 const NotFound = () => (
   <div>
+{/* <Route path='*' component={NotFound} />  */ }
     <h2> 404... This page is not found</h2>
     <p>Whoops! Sorry, there is nothing to see here</p>
     <p><Link to="/">Back to Home</Link></p>
