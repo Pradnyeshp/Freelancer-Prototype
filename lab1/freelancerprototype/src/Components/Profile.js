06182a2f71d0825120c9ada0c71f67ba163237d7
pradnyeshp <pradnyesh.patil@sjsu.edu>
2018-03-08 16:44:10 -0800

    Added Profile Updation
    Added Profile Updation

--- lab1/freelancerprototype/src/Components/Profile.js
+++ lab1/freelancerprototype/src/Components/Profile.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
 import {Link} from 'react-router-dom';
+import { connect } from 'react-redux';
+import axios from 'axios'
 
 class Profile extends Component {
 
     constructor() {
         super();
-        this.handleSubmit = this.handleSubmit.bind(this);
+
+        this.state={
+            name : "",
+            email : "",
+            phone : "",
+            aboutme : "",
+            skills : "", 
+            image : ""
+        }
+        this.handleSave = this.handleSave.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
-    handleSubmit = () =>
-    {
-        //sessionStorage.clear() ;
-        sessionStorage.removeItem('username');
-        console.log(sessionStorage);        
+    handleChange = (e) => {
+        e.preventDefault();
+        this.setState=({
+            [e.target.name] : [e.target.value]
+        })
+    }
+
+    handleSave = () => {
+        const profile = {
+            name : this.state.name,
+            email: this.state.email,
+            phone: this.state.phone,
+            aboutme: this.state.aboutme,
+            skills: this.state.skills,
+            image: this.state.image
+        }
+        this.props.profileUpdate(profile);
     }
 
     render() {
@@ -21,29 +45,65 @@ class Profile extends Component {
             <div className="profile">
                 <form>
                     <label>Name :
-                        <input type="text" ref="name"  />
+                        <input type="text" ref="name"
+                            value={this.state.value}
+                            onChange={this.handleChange}  />
                     </label><br/>
                     <label>Email :
-                        <input type="text" ref="email"  />
+                        <input type="text" ref="email"
+                            value={this.state.value}
+                            onChange={this.handleChange}  />
                     </label><br />
                     <label>Phone Number :
-                        <input type="text" ref="phone"  />
+                        <input type="text" ref="phone" 
+                            value={this.state.value}
+                            onChange={this.handleChange} />
                     </label><br />
                     <label>About Me :
-                        <input type="text" ref="aboutme" />
+                        <input type="text" ref="aboutme" 
+                            value={this.state.value}
+                            onChange={this.handleChange} />
                     </label><br />
                     <label>Skills :
-                        <input type="text" ref="email"  />
+                        <input type="text" ref="email"
+                            value={this.state.value}
+                            onChange={this.handleChange}  />
                     </label><br />
                     <label>Profile Image :
                         <input type="text" ref="email"  />
                     </label><br />
-                    <button>Edit</button><br/>
-                    <Link to='/' className='btn btn-danger' onClick={this.handleSubmit}>LogOut</Link>
+                    <button className='btn btn-primary'
+                        onClick={this.handleSave} > Save 
+                    </button>
                 </form>
             </div>
         );
     }
 }
 
-export default Profile;
+function mapStateToProps(state) {
+    return {
+        profile_updation: state.profile_updation
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        profileUpdate : (profile) => {
+            console.log("In profile Updation", profile);
+            axios.post('http://localhost:3001/profile', profile)
+                .then((response) => {
+                    console.log(response.data[0].Username);
+                    if (response.data === 'ERROR')
+                        dispatch({ type: 'ERROR', payload: response })
+                    else {
+                        sessionStorage.setItem('username', response.data[0].username)
+                        dispatch({ type: 'PROFILE_UPDATE', payload: response })
+                    }
+                }
+                );
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps) (Profile);
