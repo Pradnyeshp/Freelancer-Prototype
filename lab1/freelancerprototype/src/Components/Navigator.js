d65e40fa7c1dd183104ea8c5f18a762c600322ad
pradnyeshp <pradnyesh.patil@sjsu.edu>
2018-03-09 18:31:11 -0800

    Minor UI Changes
    Minor UI Changes

--- lab1/freelancerprototype/src/Components/Navigator.js
+++ lab1/freelancerprototype/src/Components/Navigator.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import {Link} from 'react-router-dom'
 import './navigator.css'
 
@@ -14,16 +13,10 @@ class Navigator extends Component {
                             </div>
                             <ul className="nav navbar-nav navbar-right" >
                                 <li>
-                                    <Link to="/SignIn"><font color="black"> Login </font></Link>
-                                </li>
-                            </ul>
-                            <ul className="nav navbar-nav navbar-right">
-                                <li>
-                                    <Link to="/SignUp"><font color="black"> Sign Up </font></Link>
-                                </li>
-                            </ul>
-                            <ul className="nav navbar-nav navbar-right">
-                                <li>
+                                    <Link to="/SignIn"><font color="black"> Login </font></Link> &nbsp;
+                           
+                                    <Link to="/SignUp"><font color="black"> Sign Up </font></Link> &nbsp;
+
                                     <Link to="/AddProject" className="btn btn-primary"> Post a Project </Link>
                                 </li>
                             </ul>
