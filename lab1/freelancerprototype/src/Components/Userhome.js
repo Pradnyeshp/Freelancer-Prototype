1d29a2f313ecafda48009a74407f56fc8ca6708c
pradnyeshp <pradnyesh.patil@sjsu.edu>
2018-03-11 21:32:43 -0700

    Added Navbar
    Added Navbar

--- lab1/freelancerprototype/src/Components/Userhome.js
+++ lab1/freelancerprototype/src/Components/Userhome.js
@@ -28,22 +28,26 @@ class Userhome extends Component {
                             </ul>
                         </div>
                     </nav>
-                </div>
-                <nav className="navbar navbar-inverse">
-                    <div className="container-fluid">
-                        <ul className="nav navbar-nav"> &nbsp;
-
-                            <li className="active"><Link to="#">Home</Link> &nbsp; &nbsp;
-                                <Link to="#">Dashboard</Link>
+                </div> <br/>
+              
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">               
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a> 
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Dashboard</a>
                             </li>
                         </ul>
-                        <div className="navbar-btn">
-                            <Link to='/AddProject' 
-                                className='btn btn-warning' > Post a Project
-                            </Link>
-                        </div>
-                    </div>
-                </nav>
+                        <form class="form-inline my-2 my-lg-0">
+                                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Post a Project</button>
+    </form>
+  </div>
+</nav>
             </div>
         );
     }
