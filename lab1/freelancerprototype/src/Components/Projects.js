c2d138000eefdb061c211ff3c8f466c45edd62d9
pradnyeshp <pradnyesh.patil@sjsu.edu>
2018-03-12 13:00:58 -0700

    Basic Project Component
    Basic Project Component

---
+++ lab1/freelancerprototype/src/Components/Projects.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+    render() {
+        return (
+            <div className="projects"><br/>
+                <div>
+                    Project Content will be displayed Here
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Projects;
