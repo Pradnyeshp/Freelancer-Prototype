d47ee26841ff65963684a229cc6959d6d67c0cbe
pradnyeshp <pradnyesh.patil@sjsu.edu>
2018-03-12 21:56:17 -0700

    Tagnames updated
    Tagnames updated

--- lab1/freelancerprototype/src/Components/Userhome.js
+++ lab1/freelancerprototype/src/Components/Userhome.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom'
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 //import axios from 'axios';
 import { connect } from 'react-redux';
-import Navbar from './Navbar';
 
 class Userhome extends Component {
 
@@ -30,21 +29,21 @@ class Userhome extends Component {
                     </nav>
                 </div> <br/>
               
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">               
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">               
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav mr-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#"> Home <span class="sr-only">(current)</span></a> 
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link" href=""> Home <span className="sr-only">(current)</span></a> 
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Dashboard</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="">Dashboard</a>
                             </li>
                         </ul>
-                        <form class="form-inline my-2 my-lg-0">
-                                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+                        <form className="form-inline my-2 my-lg-0">
+                                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
                                     <Link to='/AddProject' > Post a Project </Link>
                                 </button>
                         </form>
@@ -56,11 +55,16 @@ class Userhome extends Component {
 }
 
 function mapStateToProps(state) {
+    return{
+
+    }
 
 }
 
 function mapDispatchToProps(dispatch) {
+    return{
 
+    }
 }
 
 export default connect(mapStateToProps,mapDispatchToProps) (Userhome);
