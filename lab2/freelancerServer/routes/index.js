d6ad9842efc578a4cfc8b080c5b82e907351d1cb
Pradnyesh Patil <36009646+Pradnyeshp@users.noreply.github.com>
2018-03-28 01:28:18 -0700

    Query for displaying relevant projects
    Query for displaying relevant projects

--- lab2/freelancerServer/routes/index.js
+++ lab2/freelancerServer/routes/index.js
@@ -208,6 +208,53 @@ router.post('/signin', function (req, res, next) {
   // })
 });
 
+router.post('/getrelevantprojects', (req, res, next) => {
+    console.log("In Get Relevant Projects", req.body)
+
+    MongoClient.connect(url, (err, db) => {
+        if(err) throw err
+        else {
+            let dbo = db.db('freelancer')
+            dbo.collection('users').findOne( {username: req.body.username}, (err, result) => {
+                if(err) throw err
+                else {
+                    console.log("User Details in relevant projects",result);
+                    let count = 0
+                    const userSkills = result.skills.toString()
+                    const userSkillsArray = userSkills.split(",")
+                    const relevantProjectArray = []
+                    // let userSkillsArray = userSkills.toArray()
+                    console.log('User Skills : ',userSkillsArray)
+                    dbo.collection('projects').find({}).toArray( (err,result) => {
+                        if(err) throw err
+                        else {
+                            // console.log('In rel projects, all projects:',result)
+                            const allProjectsArray = result;
+                            console.log("Skills required in Projects",allProjectsArray)
+                            for( let i=0; i < userSkillsArray.length ; i++) {
+                                for( let j=0; j < allProjectsArray.length; j++) {
+                                    let allProjectSkillsArray = allProjectsArray[j].skillsreq.toString().split(',')
+                                    for( let k=0; k < allProjectSkillsArray.length ; k++) {
+                                        if(userSkillsArray[i].toLocaleLowerCase() === allProjectSkillsArray[k].toLocaleLowerCase()) {
+                                            count++;
+                                            console.log('Count is : ' , count )
+                                        }
+                                    }
+                                    if(count>=3) {
+                                        relevantProjectArray.push(allProjectsArray[j]);
+                                        console.log("final Relevant project array", relevantProjectArray)
+                                    }
+                                }
+                            }
+                            res.json(relevantProjectArray)
+                        }
+                    })
+                }
+            } )
+        }
+    })
+})
+
 router.post('/getprofile', (req, res, next) => {
   const username = req.body.username
   console.log(username);
