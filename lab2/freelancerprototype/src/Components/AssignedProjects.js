3d8306aa430c59dd8d2eae6e149960c3de09ec11
Pradnyesh Patil <36009646+Pradnyeshp@users.noreply.github.com>
2018-03-31 02:08:41 -0700

    New component Assigned Projects
    New component Assigned Projects

---
+++ lab2/freelancerprototype/src/Components/AssignedProjects.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class AssignedProjects extends Component {
+    render() {
+        return (
+            <div className="assignedprojects">
+
+            </div>
+        );
+    }
+}
+
+export default AssignedProjects;
