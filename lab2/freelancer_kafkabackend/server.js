040f070d5797e3ddb6310131c0125f59d8f6e582
Pradnyesh Patil <36009646+Pradnyeshp@users.noreply.github.com>
2018-04-03 23:51:41 -0700

    Added Consumer getprojects
    Added Consumer getprojects

--- lab2/freelancer_kafkabackend/server.js
+++ lab2/freelancer_kafkabackend/server.js
@@ -1,8 +1,10 @@
-var connection =  new require('./kafka/Connection');
-var login = require('./services/login');
+let connection =  new require('./kafka/Connection');
+let login = require('./services/login');
 const signup = require('./services/signup');
-const getprofile = require('./services/getprofile')
-let updateprofile = require('./services/updateprofile')
+const getprofile = require('./services/getprofile');
+let updateprofile = require('./services/updateprofile');
+let getprojects = require('./services/getprojects');
+
 
 let consumerlogin = connection.getConsumer('login_topic');
 let producer = connection.getProducer();
@@ -92,6 +94,31 @@ consumerupdateprofile.on('message', function (message) {
     updateprofile.handle_request( data.data, function(err,res) {
         console.log('after handle', res );
         var payloads = [
+            { topic: data.replyTo,
+                messages:JSON.stringify({
+                    correlationId:data.correlationId,
+                    data : res
+                }),
+                partition : 0
+            }
+        ];
+        producer.send(payloads, function(err, data){
+            console.log(data);
+        });
+        return;
+    });
+});
+
+let consumergetprojects = connection.getConsumer('getprojects');
+consumergetprojects.on('message', function (message) {
+    console.log('message received');
+    console.log(JSON.stringify(message.value));
+    let data = JSON.parse(message.value);
+    console.log('Data after parsing', data);
+    console.log('Data after parsing printing data only', data.data);
+    getprojects.handle_request( data.data, function(err,res) {
+        console.log('after handle', res );
+        let payloads = [
             { topic: data.replyTo,
                 messages:JSON.stringify({
                     correlationId:data.correlationId,
