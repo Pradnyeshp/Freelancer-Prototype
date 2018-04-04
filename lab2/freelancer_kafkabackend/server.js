c1bebbaa09f476c6a2ba99729754912c9d10b325
Pradnyesh Patil <36009646+Pradnyeshp@users.noreply.github.com>
2018-04-03 23:30:18 -0700

    Added consumer for update profile API
    Added consumer for update profile API

--- lab2/freelancer_kafkabackend/server.js
+++ lab2/freelancer_kafkabackend/server.js
@@ -2,9 +2,10 @@ var connection =  new require('./kafka/Connection');
 var login = require('./services/login');
 const signup = require('./services/signup');
 const getprofile = require('./services/getprofile')
+let updateprofile = require('./services/updateprofile')
 
-var consumerlogin = connection.getConsumer('login_topic');
-var producer = connection.getProducer();
+let consumerlogin = connection.getConsumer('login_topic');
+let producer = connection.getProducer();
 
 console.log('server is running');
 consumerlogin.on('message', function (message) {
@@ -31,7 +32,7 @@ consumerlogin.on('message', function (message) {
     });
 });
 
-var consumersignup = connection.getConsumer('signup_topic');
+let consumersignup = connection.getConsumer('signup_topic');
 consumersignup.on('message', function (message) {
     console.log('message received');
     console.log(JSON.stringify(message.value));
@@ -56,14 +57,39 @@ consumersignup.on('message', function (message) {
     });
 });
 
-var consumergetprofile = connection.getConsumer('getprofile');
+let consumergetprofile = connection.getConsumer('getprofile');
 consumergetprofile.on('message', function (message) {
     console.log('message received');
     console.log(JSON.stringify(message.value));
-    var data = JSON.parse(message.value);
+    let data = JSON.parse(message.value);
     console.log('Data after parsing', data);
-    console.log('Data after parsing priting data only', data.data);
+    console.log('Data after parsing printing data only', data.data);
     getprofile.handle_request(data.data, function(err,res){
+        console.log('after handle', res );
+        var payloads = [
+            { topic: data.replyTo,
+                messages:JSON.stringify({
+                    correlationId:data.correlationId,
+                    data : res
+                }),
+                partition : 0
+            }
+        ];
+        producer.send(payloads, function(err, data){
+            console.log(data);
+        });
+        return;
+    });
+});
+
+let consumerupdateprofile = connection.getConsumer('updateprofile');
+consumerupdateprofile.on('message', function (message) {
+    console.log('message received');
+    console.log(JSON.stringify(message.value));
+    let data = JSON.parse(message.value);
+    console.log('Data after parsing', data);
+    console.log('Data after parsing printing data only', data.data);
+    updateprofile.handle_request( data.data, function(err,res) {
         console.log('after handle', res );
         var payloads = [
             { topic: data.replyTo,
