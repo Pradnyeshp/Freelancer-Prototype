f646c4907ed6371e2b56c3bc04a1403556504248
Pradnyesh Patil <36009646+Pradnyeshp@users.noreply.github.com>
2018-04-06 10:09:09 -0700

    Created service in kafka for API withdrawmoney
    Created service in kafka for API withdrawmoney

---
+++ lab2/freelancer_kafkabackend/services/withdrawmoney.js
@@ -0,0 +1,33 @@
+let mongo = require('./mongo');
+
+function handle_request( msg, callback) {
+
+    console.log("In handle request:"+ JSON.stringify(msg));
+
+    mongo.connect( (err, db) => {
+        if(err) throw err;
+        else {
+            db.collection('users').updateOne( { username : msg.username }, { $set : { balance : msg.amount } }, (err, result) => {
+                if(err) throw err;
+                else {
+                    callback(null, result)
+                }
+            });
+
+            //Updating Transactions Table
+            db.collection('transaction').insertOne({
+                id : msg.id,
+                pname : msg.pname,
+                amount : msg.debit,
+                transType : 'debit',
+                username : msg.username,
+                date : new Date()
+            }, (err, result) => {
+                if(err) throw err;
+                console.log('Updated Transaction History')
+            })
+        }
+    })
+}
+
+exports.handle_request = handle_request;
