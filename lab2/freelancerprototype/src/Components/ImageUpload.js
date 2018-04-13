7c796f66a48e0119e429152af0f124fbbac55969
Pradnyesh Patil <36009646+Pradnyeshp@users.noreply.github.com>
2018-04-12 23:07:58 -0700

    Updated component for image upload
    Updated component for image upload

--- lab2/freelancerprototype/src/Components/ImageUpload.js
+++ lab2/freelancerprototype/src/Components/ImageUpload.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
+import url from '../serverurl';
 //import image from '../images/freelancerlogo.png';
 
 class Imageuploader extends Component {
@@ -9,48 +10,103 @@ class Imageuploader extends Component {
         this.state = {
             fileSelected: '',
             imagePreview: '',
-            display: 'none'
+            display: 'none',
+            filename: 'default.png'
         }
+    }
+
+    componentWillMount(){
+
+        this.loadUserImage();
+
+    }
+
+    loadUserImage(){
+
+        var self = this;
+        axios.get( url + "/getuserimage?username=" + localStorage.getItem('username'), {withCredentials: true} )
+            .then( (response) => {
+                console.log('In imageupload', response.data);
+                self.setState({
+                    filename: response.data.image_name.image_name
+                })
+            })
     }
 
     handleChange = e => {
         e.preventDefault();
-        console.log("bhdib", e.target.files[0].name);
-        let rdr = new FileReader();
-        let fileSelected = e.target.files[0];
+        console.log("In handle Change", e.target.files[0].name);
+        var rdr = new FileReader();
+        var fileSelected = e.target.files[0];
         rdr.onloadend = () => {
             this.setState({
-                fileSelected: fileSelected,
-                imagePreview: rdr.result,
-                display: 'block'
+                // fileSelected: fileSelected,
+                // imagePreview: rdr.result,
+                display: 'block',
+                fileSelected: fileSelected
             });
         }
 
         rdr.readAsDataURL(fileSelected);
     }
 
-    handleUplaod = () => {
-        console.log("In handle upload...")
-        console.log("In mapDispatch" + this.state.fileSelected + "......" + this.state.imagePreview);
+    handleUplaod = (e) => {
+        e.preventDefault();
+        const form_data = new FormData();
+        form_data.append('filevalue', this.state.fileSelected);
+        form_data.append('username', localStorage.getItem('username'));
+        console.log(form_data);
+        const config = {
+            headers: {
+                'content-type': 'multipart/form-data'
+            }
+        }
+
+        if(this.state.fileSelected !== ""){
+            var self =this;
+            axios.post(url + '/saveimage', form_data, { withCredentials: true}, config)
+                .then( (response) => {
+                    self.setState({
+                        filename: "" + localStorage.getItem('username') + "." + response.data.fileType
+
+                    })
+                })
+        }
+        else{
+            alert("Please upload an image first");
+        }
+        // console.log("In handle upload...")
+        // console.log("In mapDispatch",  this.state.fileSelected + "......" + this.state.imagePreview);
+        // const imageDetails = {
+        //     fileSelected: this.state.fileSelected,
+        //     imagePreview: this.state.imagePreview
+        // }
+        // axios.post('http://localhost:3001/saveimage', imageDetails, { withCredentials: true})
+        // .then( (response) => {
+
+        // })
     }
 
 
     render() {
-        let { imagePreview } = this.state;
+
+        // let {imagePreview} = this.state;
         let $imagePreviewFinal = null;
-        if (imagePreview) {
-            $imagePreviewFinal = (<img src={imagePreview} alt="This is user's display pic" />);
+        if (this.state.filename) {
+
+            // $imagePreviewFinal = (<img src = { require('/Users/venkateshdevale/Desktop/private git/cmpe273/lab2/Freelancer/freelancer-server/images/' + this.state.filename) } alt = "This is user's display pic"/>);
+            $imagePreviewFinal = (<img src = { require('E:/CMPE273/lab2/freelancerServer/public/' + this.state.filename) } alt = "This is display pic"/>);
         }
         const style = {
-            display: this.state.display
+            display : this.state.display
         }
-        return (
+        return(
             <div className="Imageuploader">
                 <div id='profileImage'>
                     {$imagePreviewFinal}
                     <div id='imageUploader'>
-                        <input type='file' className='fileInput' onChange={this.handleChange} />
-                        <button style={style} onClick={this.handleUplaod} className="btn btn-primary"><label>Upload</label></button>
+                        <input type='file' className='fileInput' onChange={this.handleChange.bind(this)} />
+                        <button style = {style} onClick = {this.handleUplaod.bind(this)} className="btn btn-primary"><label>Upload</label></button>
                     </div>
                 </div>
             </div>
@@ -61,11 +117,14 @@ class Imageuploader extends Component {
 
 function mapDispatchToProps(dispatch) {
     return {
-        saveImage: (image) => {
+        saveImage : (image) => {
             //console.log("In mapDispatch" + imageDetails + "......" + imageName);
-            axios.post('http://localhost:3001/saveImage', image)
-                .then((response) => {
-
+            axios.post(url + '/saveImage', image)
+                .then( (response) => {
+                    if(response.data.message === 'Image Uploaded')
+                        alert(response.data.message);
+                    else
+                        alert('Error in updating image...try again later');
                 })
         }
     }
