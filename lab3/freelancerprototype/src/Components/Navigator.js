a57f1ad72b516c03c8b5f7cd4f2fc6073b236efb
Pradnyesh Patil <36009646+Pradnyeshp@users.noreply.github.com>
2018-05-08 00:59:44 -0700

    Updated landing page to look like freelancer.com
    Updated landing page to look like freelancer.com

--- lab3/freelancerprototype/src/Components/Navigator.js
+++ lab3/freelancerprototype/src/Components/Navigator.js
@@ -14,7 +14,7 @@ class Navigator extends Component {
         }
 
         return (
-            <div className="container-fluid" > <br/>
+            <div className="homepage" >
                 {profile}
                     <nav className="navbar navbar-inverse" >
                     <Helmet>
@@ -26,20 +26,31 @@ class Navigator extends Component {
                             </div>
                             <ul className="nav navbar-nav navbar-right" >
                                 <li>
-                                <Link to="/signIn"><font color="black">Log In</font></Link> &nbsp;&nbsp;&nbsp;           
-                                <Link to="/signUp"><font color="black">Sign Up</font></Link> &nbsp;&nbsp;&nbsp;
-                                <Link to="/addproject" className="btn btn-primary"> Post a Project </Link>
+                                    <button className='btn'>
+                                        <Link to="/signIn"><font color="black">Log In</font></Link>
+                                    </button> &nbsp;&nbsp;&nbsp;
+                                    <button className='btn'>
+                                        <Link to="/signUp"><font color="black">Sign Up</font></Link>
+                                    </button> &nbsp;&nbsp;&nbsp;
+                                <Link to="/addproject" className="btn btn-info"> Post a Project </Link>
                                 </li>
                             </ul>
                         </div>
                     </nav>
                 <div className='ftext' >
                     <div className='text-left' ><br /><br /><br /><br /><br />
-                        <h1> Hire expert freelancers for any </h1>
-                        <h1> job, online </h1> <br/>
-                        <h6> Millions of small businesses use Freelancer to turn their ideas </h6>
-                        <h6> into reality. </h6>
+                        {/*<h1> Hire expert freelancers for any </h1>*/}
+                        {/*<h1> job, online </h1> <br/>*/}
+                        {/*<h6> Millions of small businesses use Freelancer to turn their ideas </h6>*/}
+                        {/*<h6> into reality. </h6>*/}
                         </div>
+                    <br /><br /><br /><br /><br />
+                    <br /><br /><br /><br /><br />
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
                 </div>
             </div>
         );
